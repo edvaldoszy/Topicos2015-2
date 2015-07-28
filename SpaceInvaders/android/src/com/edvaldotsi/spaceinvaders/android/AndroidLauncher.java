73008ac0a126f670c30a20350653577786f3864e
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-07-27 21:09:54 -0300

    Enviando arquivos do projeto

---
+++ SpaceInvaders/android/src/com/edvaldotsi/spaceinvaders/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.edvaldotsi.spaceinvaders.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.edvaldotsi.spaceinvaders.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
---
+++ SpaceInvaders/desktop/src/com/edvaldotsi/spaceinvaders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.edvaldotsi.spaceinvaders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.edvaldotsi.spaceinvaders.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
